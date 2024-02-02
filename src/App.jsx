e35e2e9fbeada6544b287793bfbe30b270de0212
ixtk <ikhcamot@gmail.com>
2024-02-02 22:43:04 +0400

    Add carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react"
 import "./App.css"
 import { Header } from "./components/Header/Header"
+import { ImageCarousel } from "./components/Carousel/ImageCarousel"
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <main>
+        <ImageCarousel />
+      </main>
     </div>
   )
 }
---
+++ src/components/Carousel/ImageCarousel.jsx
@@ -0,0 +1,22 @@
+import "./ImageCarousel.css"
+import prevIconUrl from "../../assets/icon-previous.svg"
+import nextIconUrl from "../../assets/icon-next.svg"
+
+export const ImageCarousel = () => {
+  return (
+    <div className="carousel">
+      <button className="control-btn prev">
+        <img src={prevIconUrl} alt="Previous icon" />
+      </button>
+      <div className="img-container">
+        <img
+          src="https://res.cloudinary.com/dc2c49xov/image/upload/v1703352357/ecommerce-page/image-product-1.jpg"
+          alt=""
+        />
+      </div>
+      <button className="control-btn next">
+        <img src={nextIconUrl} alt="Next icon" />
+      </button>
+    </div>
+  )
+}
