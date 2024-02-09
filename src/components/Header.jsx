3b7b97fabfb82fd898b6e8e27a9be733f9ef3c64
ixtk <ikhcamot@gmail.com>
2024-02-09 12:11:58 +0400

    Add menu, cart & active img states

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import menuIconUrl from "../assets/icon-menu.svg"
 import logoUrl from "../assets/logo.svg"
 import { cartIcon } from "./CartIcon"
@@ -5,18 +6,33 @@ import deleteUrl from "../assets/icon-delete.svg"
 import closeIcon from "../assets/icon-close.svg"
 import { assetsBaseUrl, loggedInUser, product } from "../data"
 import "../styles/Header.css"
+import clsx from "clsx"
 
 export const Header = () => {
+  const [cartVisible, setCartVisible] = useState(false)
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false)
+
+  useEffect(() => {
+    if (mobileMenuVisible) {
+      document.body.style.overflow = "hidden"
+    } else {
+      document.body.style.overflow = "auto"
+    }
+  }, [mobileMenuVisible])
+
   return (
     <header>
       <div className="header-col">
-        <button className="menu-btn">
+        <button className="menu-btn" onClick={() => setMobileMenuVisible(true)}>
           <img src={menuIconUrl} alt="Menu icon" />
         </button>
         <img className="logo" src={logoUrl} alt="Sneakers logo" />
-        <div className="mobile-menu">
+        <div className={clsx("menu", mobileMenuVisible && "visible")}>
           <nav>
-            <button className="close-btn">
+            <button
+              className={clsx("close-btn", mobileMenuVisible && "visible")}
+              onClick={() => setMobileMenuVisible(false)}
+            >
               <img src={closeIcon} alt="" />
             </button>
             <ul className="nav-list">
@@ -37,34 +53,44 @@ export const Header = () => {
               </li>
             </ul>
           </nav>
-          <button></button>
+          <button
+            className="overlay-btn"
+            onClick={() => setMobileMenuVisible(false)}
+          ></button>
         </div>
       </div>
 
       <div className="header-col cart-reference">
-        <button className="cart-btn">{cartIcon}</button>
-        <div className="cart">
-          <h3>Cart</h3>
-          <div className="cart-list">
-            <div className="cart-item">
-              <img
-                className="cart-product-img"
-                src={`${assetsBaseUrl}/${product.images.thumbnails[0]}`}
-                alt=""
-              />
-              <div>
-                <p>Fall Limited Edition Sneakers</p>
-                <span>
-                  $125.00 x 3 <strong>$375.00</strong>
-                </span>
+        <button
+          className="cart-btn"
+          onClick={() => setCartVisible(!cartVisible)}
+        >
+          {cartIcon}
+        </button>
+        {cartVisible && (
+          <div className="cart">
+            <h3>Cart</h3>
+            <div className="cart-list">
+              <div className="cart-item">
+                <img
+                  className="cart-product-img"
+                  src={`${assetsBaseUrl}/${product.images.thumbnails[0]}`}
+                  alt=""
+                />
+                <div>
+                  <p>Fall Limited Edition Sneakers</p>
+                  <span>
+                    $125.00 x 3 <strong>$375.00</strong>
+                  </span>
+                </div>
+                <button>
+                  <img src={deleteUrl} alt="" />
+                </button>
               </div>
-              <button>
-                <img src={deleteUrl} alt="" />
-              </button>
+              <button className="checkout-btn btn-primary">Checkout</button>
             </div>
-            <button className="checkout-btn btn-primary">Checkout</button>
           </div>
-        </div>
+        )}
         <img
           className="avatar-img"
           src={`${assetsBaseUrl}/${loggedInUser.profileImage}`}
--- src/components/ImageCarousel.jsx
+++ src/components/ImageCarousel.jsx
@@ -1,28 +1,56 @@
+import { useState } from "react"
 import "../styles/ImageCarousel.css"
 import prevIconUrl from "../assets/icon-previous.svg"
 import nextIconUrl from "../assets/icon-next.svg"
 import { assetsBaseUrl, product } from "../data"
+import clsx from "clsx"
 
 export const ImageCarousel = () => {
+  const [activeImgIndex, setActiveImgIndex] = useState(0)
+
+  const showNextImg = () => {
+    if (activeImgIndex === product.images.originals.length - 1) {
+      setActiveImgIndex(0)
+    } else {
+      setActiveImgIndex(activeImgIndex + 1)
+    }
+  }
+
+  const showPrevImg = () => {
+    if (activeImgIndex === 0) {
+      setActiveImgIndex(product.images.originals.length - 1)
+    } else {
+      setActiveImgIndex(activeImgIndex - 1)
+    }
+  }
+
+  const changeImgIndex = (index) => {
+    setActiveImgIndex(index)
+  }
+
   return (
     <div className="carousel">
-      <button className="control-btn prev">
+      <button className="control-btn prev" onClick={showPrevImg}>
         <img src={prevIconUrl} alt="Previous icon" />
       </button>
       <div className="img-container">
         <img
           className="active"
-          src="https://res.cloudinary.com/dc2c49xov/image/upload/v1703352357/ecommerce-page/image-product-1.jpg"
+          src={`${assetsBaseUrl}/${product.images.originals[activeImgIndex]}`}
           alt=""
         />
       </div>
-      <button className="control-btn next">
+      <button className="control-btn next" onClick={showNextImg}>
         <img src={nextIconUrl} alt="Next icon" />
       </button>
       <div className="thumbnails">
-        {product.images.thumbnails.map((thumbnail, index) => {
+        {product.images.thumbnails.map((thumbnail, thumbnailIndex) => {
           return (
-            <button key={index} className={index === 1 && "active"}>
+            <button
+              key={thumbnailIndex}
+              className={clsx(thumbnailIndex === activeImgIndex && "active")}
+              onClick={() => changeImgIndex(thumbnailIndex)}
+            >
               <img src={`${assetsBaseUrl}/${thumbnail}`} alt="" />
             </button>
           )
